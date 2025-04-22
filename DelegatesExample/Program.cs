b93a8d9adb7e2f379d377f7d25a6d02ec19d314c
Supriya9088 <yadavsupriya.90@gmail.com>
2025-04-22 11:10:51 +0530

    first commit

---
+++ DelegatesExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public delegate int Calculation(int numOne, int numTwo);
+    class Mathematics
+{
+    static int Addition(int numOne, int numTwo)
+    { 
+        return (numOne + numTwo);
+    }
+    static int Substraction(int numOne, int numTwo)
+    {
+        return (numOne - numTwo);
+    }
+    static void Main(string[] args)
+    {
+        int valOne = 5;
+        int valTwo = 23;
+        Calculation objCalculation = new Calculation(Addition);
+        Console.WriteLine(valOne + "+" + valTwo + "=" + objCalculation(valOne,valTwo));
+        Calculation objCal=new Calculation(Substraction);
+        Console.WriteLine(valOne + "-" + valTwo + "=" + objCal(valOne, valTwo));
+    }
+}
+
